60e5e27508ad3155e2589c7f53941efeeb31e654
Abora <mahmod.elebiary@gmail.com>
2022-06-11 02:21:35 +0200

    make nfcHandler

--- app/src/main/java/com/abora/nfcreader/MainActivity.kt
+++ app/src/main/java/com/abora/nfcreader/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.abora.nfcreader
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.PendingIntent
+import android.content.Intent
+import android.nfc.NdefMessage
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.Ndef
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var nfcAdapter: NfcAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
     }
+
+
+    override fun onResume() {
+        super.onResume()
+        NfcUtil.enableNFCInForeground(nfcAdapter,this,javaClass)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        NfcUtil.disableNFCInForeground(nfcAdapter,this)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        Toast.makeText(applicationContext, NfcUtil.getDataFromTag(intent), Toast.LENGTH_LONG).show()
+        NfcUtil.getDataFromTag(intent)
+    }
+
+
 }
---
+++ app/src/main/java/com/abora/nfcreader/NfcUtil.kt
@@ -0,0 +1,58 @@
+package com.abora.nfcreader
+
+import android.app.Activity
+import android.app.PendingIntent
+import android.content.Intent
+import android.nfc.NdefMessage
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.Ndef
+import android.util.Log
+import android.widget.Toast
+
+object NfcUtil {
+
+    fun <T> enableNFCInForeground(nfcAdapter: NfcAdapter, activity: Activity, classType: Class<T>) {
+        if (nfcAdapter != null){
+            val pendingIntent = PendingIntent.getActivity(
+                activity, 0,
+                Intent(activity, classType).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0
+            )
+            nfcAdapter.enableForegroundDispatch(activity, pendingIntent, null, null)
+        }else{
+            Toast.makeText(activity, "nfc not support your device.", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun disableNFCInForeground(nfcAdapter: NfcAdapter, activity: Activity) {
+        if (nfcAdapter != null)
+            nfcAdapter.disableForegroundDispatch(activity)
+    }
+
+    fun getDataFromTag(intent: Intent?): String {
+        var text = ""
+        val nfcData: Tag? = intent?.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+        val ndef = Ndef.get(nfcData)
+        try {
+            ndef.connect()
+            val messages = intent?.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+            if (messages != null) {
+                val ndefMessages = arrayOfNulls<NdefMessage>(messages.size)
+                for (i in messages.indices) {
+                    ndefMessages[i] = messages[i] as NdefMessage
+                }
+                val record = ndefMessages[0]!!.records[0]
+                val payload = record.payload
+                text = String(payload)
+                Log.e("tag", text)
+                ndef.close()
+
+            }
+            return text.toString()
+        } catch (e: Exception) {
+            Log.e("tag", "Cannot Read From Tag.")
+            return "Cannot Read From Tag."
+//            Toast.makeText(applicationContext, "", Toast.LENGTH_LONG).show()
+        }
+    }
+}
